d20af2530700263cd9d7304786fe4b72749efc27
Unknown <seki72000@gmail.com>
2019-06-17 21:01:31 +0200

    Add plugin files

---
+++ core/ajax/shutters.ajax.php
@@ -0,0 +1,29 @@
+<?php
+/* This file is part of NextDom.
+ *
+ * NextDom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NextDom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NextDom. If not, see <http://www.gnu.org/licenses/>.
+ */
+try {
+    require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+    include_file('core', 'authentification', 'php');
+    if (!isConnect('admin')) {
+        throw new Exception(__('401 - Accès non autorisé', __FILE__));
+    }
+
+    throw new Exception(__('Aucune méthode correspondante à : ', __FILE__) . init('action'));
+    
+    /***********Catch exception***************/
+} catch (Exception $e) {
+    ajax::error(displayException($e), $e->getCode());
+}
---
+++ core/class/shutters.class.php
@@ -0,0 +1,28 @@
+<?php
+
+/* This file is part of NextDom.
+ *
+ * NextDom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NextDom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NextDom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/* * ***************************Includes********************************* */
+require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+require_once 'shuttersCmd.class.php';
+if (!class_exists('Season')) { require_once dirname(__FILE__) . '/../../core/php/season.class.php'; }
+
+class shutters extends eqLogic
+{
+
+}
